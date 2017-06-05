bcf4da5ee81d0575420760c0a962c796895c2919
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2017-06-05 11:16:14 -0400

    Updated API key

--- site/config/config.php
+++ site/config/config.php
@@ -16,6 +16,7 @@ for more information: http://getkirby.com/license
 */
 
 c::set('license', 'put your license key here');
+c::set('snipcart_api_key', 'YjdiNWIyOTUtZTIyMy00MWMwLTkwNDUtMzI1M2M2NTgxYjE0')
 
 /*
 
